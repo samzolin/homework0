f1f88a4a9dab871ee5e966b088e3c2c0066bb4fe
samzolin <saz5193@psu.edu>
2020-08-28 00:46:42 +0000

    entered a capital c

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-celsius = float(input("Enter temperature in Celsius: "))
+celsius = float(input("Enter temperature in celsius: "))
 fahrenheit = (celsius * 9/5) + 32
 print("%.2f° Celsius is equivalent to: %0.2f° Fahrenheit" %(celsius, fahrenheit))
